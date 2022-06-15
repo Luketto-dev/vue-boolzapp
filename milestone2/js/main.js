6c334f8b38acd01911e6b6076fd73fc8a007f71a
Luketto-dev <luca.francesconi1995@gmail.com>
2022-06-15 17:15:45 +0200

    implementata funzione addMessage per inserire un nuovo messaggio nella chat

--- milestone2/js/main.js
+++ milestone2/js/main.js
@@ -5,6 +5,17 @@
 //Visualizzazione dinamica della lista contatti: tramite la direttiva
 //v-for, visualizzare nome e immagine di ogni contatto
 
+//Milestone 2
+//Visualizzazione dinamica dei messaggi: tramite la direttiva v-for,
+//visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+//Click sul contatto mostra la conversazione del contatto cliccato
+
+//Milestone 3
+//Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” 
+//il testo viene aggiunto al thread sopra, come messaggio verde
+//Risposta dall’interlocutore: ad ogni inserimento di un messaggio, 
+//l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
 
 const contatti = [
   {
@@ -197,6 +208,7 @@ const boolZapp = new Vue({
         },
       ],
     },
+    newMessage:''
   },
   methods:{
     getUrls: function(idImg){
@@ -205,6 +217,14 @@ const boolZapp = new Vue({
     getActiveContact: function(activeContact){
       this.currentContact = activeContact
       return this.currentContact
+    },
+    addMessage: function(){
+      this.newMessage = {
+        date: "10/01/2020 15:30:55",
+        message: this.newMessage,
+        status: "sent",
+      }
+      this.currentContact.messages.push(this.newMessage)
     }
   }
 })
