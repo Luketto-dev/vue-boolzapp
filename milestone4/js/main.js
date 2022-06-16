f0ba17ada9ccb754aef4814941f0fbe97a39aefc
Luketto-dev <luca.francesconi1995@gmail.com>
2022-06-16 12:50:52 +0200

    correzioni post-lezione, implementazione funzione per filtrare gli utenti

--- milestone4/js/main.js
+++ milestone4/js/main.js
@@ -17,6 +17,11 @@
 //Risposta dall’interlocutore: ad ogni inserimento di un messaggio, 
 //l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
+//Milestone 4
+//Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+//contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+//“mar” rimangono solo Marco e Martina)
+
 
 const contatti = [
   {
@@ -188,6 +193,7 @@ const boolZapp = new Vue({
     contacts: contatti,
     currentContact: contatti[0],
     newMessageInputText:'',
+    inputSearch: '',
   },
 
   methods:{
@@ -219,6 +225,16 @@ const boolZapp = new Vue({
         status: "received",
       })
     },
+
+    getContactFiltered: function(){
+      return this.contacts.filter((contact) => {
+        if (contact.name.includes(this.inputSearch)) {
+          return true
+        }
+
+        
+      })
+    }
 
 
   }
